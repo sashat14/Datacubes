d999fdfadc044b4e25fd2ee262561a7d5ff4146d
Sasha <taylork85@gmail.com>
2019-11-07 15:56:46 -0600

    create tables

---
+++ backend/data/migrations/20191107154252_companies.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('companies', tbl => {
+    tbl.increments().unique().notNullable().primary()
+    tbl.string("name")
+    tbl.string("address")
+  })
+}
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExist('companies');
+};
